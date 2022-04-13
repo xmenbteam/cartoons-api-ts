7732e61c5252d1ca7a722c0514a8e8ba81c08fae
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2022-04-13 12:05:28 +0100

    Heroku config

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,10 +6,20 @@ require("dotenv").config({
   path: `${__dirname}/../../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE NOT SET");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const db = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+const db = new Pool(config);
 
 export default db;
